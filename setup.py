a89ecbcc3c926bc4b681c1f1919f201a14ebdffd
YorkSu <york_su@qq.com>
2021-05-30 01:50:46 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startsw
 
 setup(
     name='kitpy',
-    version='0.2.3',
+    version='0.2.4',
     description='Kit of Python',
     author='York Su',
     author_email='york_su@qq.com',
@@ -45,3 +45,4 @@ setup(
     ],
     license="MIT license",
 )
+
