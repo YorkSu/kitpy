82f512ab374ec9f9ec01563267bca49f241355e1
YorkSu <york_su@qq.com>
2021-05-30 00:39:11 +0800

    v0.2.1

--- kitpy/__init__.py
+++ kitpy/__init__.py
@@ -5,24 +5,46 @@ from kitpy import convert
 from kitpy import flags
 from kitpy import log
 from kitpy import path
+from kitpy import project
 from kitpy import singleton
 from kitpy import timez
 
-from kitpy.config import ConfigHandler
-from kitpy.config import JsonHandler
-from kitpy.config import YamlHandler
-from kitpy.convert import AdvancedDict
-from kitpy.convert import dict2ad
-from kitpy.flags import Flags
-from kitpy.flags import FLAGS
-from kitpy.log import Log
-from kitpy.singleton import Singleton
-from kitpy.timez import Count
+from kitpy import dev
 
 __author__ = 'York Su'
 __email__ = 'york_su@qq.com'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
+ConfigHandler = config.ConfigHandler
+JsonHandler = config.JsonHandler
+YamlHandler = config.YamlHandler
+
+AdvancedDict = convert.AdvancedDict
+dict2ad = convert.dict2ad
+
+Flags = flags.Flags
+FLAGS = flags.FLAGS
+
+DEFAULT_CFG = log.DEFAULT_CFG
+Log = log.Log
+getLogger = log.getLogger
 get_logger = log.get_logger
+
+PROJECT = project.PROJECT
+ROOT = project.ROOT
+CONFIG = project.CONFIG
+DATA = project.DATA
+set_root = project.set_root
+set_root_file = project.set_root_file
+workspace = project.workspace
+
+AbstractSingleton = singleton.AbstractSingleton
+Singleton = singleton.Singleton
+
+Count = timez.Count
+TimeFormat = timez.TimeFormat
+localtime = timez.localtime
 now = timez.now
 sleep = timez.sleep
+strftime = timez.strftime
+unix = timez.unix
