c8de922add54e4d002a12d591ae365a7156a1e4e
YorkSu <york_su@qq.com>
2021-06-01 21:58:23 +0800

    Update project.py

--- kitpy/project.py
+++ kitpy/project.py
@@ -7,7 +7,7 @@ from kitpy import path
 
 class Project(Singleton):
     def __init__(self,
-                 root: str,
+                 root: AnyStr,
                  config = 'config',
                  data = 'data'):
         self._root = root
@@ -15,15 +15,15 @@ class Project(Singleton):
         self._data = data
 
     @property
-    def root(self) -> str:
+    def root(self) -> AnyStr:
         return path.normpath(self._root)
 
     @property
-    def config(self) -> str:
+    def config(self) -> AnyStr:
         return path.normpath(path.join(self._root, self._config))
 
     @property
-    def data(self) -> str:
+    def data(self) -> AnyStr:
         return path.normpath(path.join(self._root, self._data))
 
     def workspace(self, name: AnyStr) -> AnyStr:
@@ -38,16 +38,16 @@ class Project(Singleton):
         """
         return path.normpath(path.join(self._root, name))
 
-    def set_root(self, root: str) -> None:
+    def set_root(self, root: AnyStr) -> None:
         self._root = root
 
-    def set_root_file(self, _file: str, extra='') -> None:
+    def set_root_file(self, _file: AnyStr, extra='') -> None:
         self._root = path.join(path.fd(_file), extra)
 
-    def set_config(self, config: str) -> None:
+    def set_config(self, config: AnyStr) -> None:
         self._config = config
 
-    def set_data(self, data: str) -> None:
+    def set_data(self, data: AnyStr) -> None:
         self._data = data
 
 
