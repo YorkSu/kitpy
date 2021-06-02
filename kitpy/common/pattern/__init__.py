b5f9a0d6d91f7ceed5e14e983f8a2e17f179579c
YorkSu <york_su@qq.com>
2021-06-02 20:32:12 +0800

    Update __init__.py

--- kitpy/common/pattern/__init__.py
+++ kitpy/common/pattern/__init__.py
@@ -2,3 +2,7 @@
 """Design Patterns."""
 
 from kitpy.common.pattern import singleton
+
+Singleton = singleton.Singleton
+SingletonMetaclass = singleton.SingletonMetaclass
+AbstractSingletonMetaclass = singleton.AbstractSingletonMetaclass
